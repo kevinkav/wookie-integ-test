b46b84a554244d1d75e9c334d9a886cf83a9475c
eeikkah <kevin.kavanagh@ericsson.com>
2015-04-13 15:15:24 +0100

    Increased num of tests

--- src/test/java/my/arquillian/test/PersistenceContextScopeTest.java
+++ src/test/java/my/arquillian/test/PersistenceContextScopeTest.java
@@ -47,61 +47,61 @@ public class PersistenceContextScopeTest {
 	private static final String TEST = "Test";
 	private static final String PASSED = "Passed";
 	private static final String FAILED = "Failed";
-    private static final Logger LOG = LoggerFactory.getLogger(PersistenceContextScopeTest.class);    
-  
-    @Inject
-    Ejb3xBeanLocator ejb3xBeanLocator;
-       
-    
-    
-    /* **************************************************************
-     *                Deployments Containers 1 & 2
-     **************************************************************** */
-    
+	private static final Logger LOG = LoggerFactory.getLogger(PersistenceContextScopeTest.class);    
+
+	@Inject
+	Ejb3xBeanLocator ejb3xBeanLocator;
+
+
+
+	/* **************************************************************
+	 *                Deployments Containers 1 & 2
+	 **************************************************************** */
+
 	@Deployment(name = TEST + CONTAINER_1_NON_JTS, testable = true, order = 1)
 	@TargetsContainer(CONTAINER_1_NON_JTS)
-    public static EnterpriseArchive createTestEar_Container1() {
+	public static EnterpriseArchive createTestEar_Container1() {
 		return Deployments.createTestEar();
-    }
-	
+	}
+
 	@Deployment(name = EAR_A + CONTAINER_1_NON_JTS, testable = false, order = 2)
 	@TargetsContainer(CONTAINER_1_NON_JTS)
 	public static EnterpriseArchive getEarA_Container1() {
 		return Deployments.getEarModuleA();
 	}
-	
+
 	@Deployment(name = EAR_B + CONTAINER_2_NON_JTS, testable = false, order = 3)
 	@TargetsContainer(CONTAINER_2_NON_JTS)
 	public static EnterpriseArchive getEarB_Container2() {
 		return Deployments.getEarModuleB();
 	}
-    
-    /* **************************************************************
-     *                Deployments Containers 3 & 4
-     **************************************************************** */
-    
+
+	/* **************************************************************
+	 *                Deployments Containers 3 & 4
+	 **************************************************************** */
+
 	@Deployment(name = TEST + CONTAINER_3_JTS, testable = true, order = 4)
 	@TargetsContainer(CONTAINER_3_JTS)
-    public static EnterpriseArchive createTestEar() {
+	public static EnterpriseArchive createTestEar() {
 		return Deployments.createTestEar();
-    }
-	
+	}
+
 	@Deployment(name = EAR_A + CONTAINER_3_JTS, testable = false, order = 5)
 	@TargetsContainer(CONTAINER_3_JTS)
 	public static EnterpriseArchive getEarA() {
 		return Deployments.getEarModuleA();
 	}
-	
+
 	@Deployment(name = EAR_B + CONTAINER_4_JTS, testable = false, order = 6)
 	@TargetsContainer(CONTAINER_4_JTS)
 	public static EnterpriseArchive getEarB() {
 		return Deployments.getEarModuleB();
 	}
-	
+
 	/* ***********************************************
 	 *                Test Cases
 	 ************************************************* */
-	
+
 	@Test
 	@OperateOnDeployment(TEST + CONTAINER_1_NON_JTS)
 	public void test_Ejb3x_StatelessA_Without_Jts() throws Exception {
@@ -120,8 +120,8 @@ public class PersistenceContextScopeTest {
 		}
 		LOG.info("### Finished test [1] ###");
 	}
-	
-	
+
+
 	@Test
 	@OperateOnDeployment(TEST + CONTAINER_1_NON_JTS)
 	public void test_Ejb3x_StatefulA_Without_Jts() throws Exception {
@@ -132,7 +132,7 @@ public class PersistenceContextScopeTest {
 		testCase.setUp();
 		try {
 			testCase.runTest(CONTAINER_1_OFFSET, CONTAINER_2_OFFSET);
-			fail("Should have got EJBTransactionRolledbackException here!");
+			fail("Should have got NamingException here!");
 		}catch (NamingException ex){
 			LOG.info("### Received expected exception ###");
 		}finally{
@@ -142,34 +142,113 @@ public class PersistenceContextScopeTest {
 	}
 
 	@Test
-	@OperateOnDeployment(TEST + CONTAINER_3_JTS)
-	public void test_Ejb2x_StatelessA_With_Jts() throws Exception {
+	@OperateOnDeployment(TEST + CONTAINER_1_NON_JTS)
+	public void test_Ejb2x_StatelessA_Without_Jts() throws Exception {
 		LOG.info("### Beginning test [3] ###");
 		String lookup = "ejb:ear-module-a-1.0-SNAPSHOT/ejb-module-a-1.0-SNAPSHOT/Ejb2x_StatelessA!my.test.api.TestCaseRemote";
 		TestCaseRemote testCase = (TestCaseRemote)ejb3xBeanLocator.locateBean(lookup);
 		Assert.assertNotNull(testCase);
 		testCase.setUp();
-		testCase.runTest(CONTAINER_3_OFFSET, CONTAINER_4_OFFSET);
-		testCase.tearDown();
+		try {
+			String actualResult = testCase.runTest(CONTAINER_1_OFFSET, CONTAINER_2_OFFSET);
+			Assert.assertEquals(FAILED, actualResult);
+		}catch (EJBTransactionRolledbackException ex){
+			LOG.info("### Received expected exception ###");
+		}finally{
+			testCase.tearDown();
+		}
 		LOG.info("### Finished test [3] ###");
 	}
-	
-	
+
 	@Test
-	@OperateOnDeployment(TEST + CONTAINER_3_JTS)
-	public void test_Ejb2x_StatefulA_With_Jts() throws Exception {
+	@OperateOnDeployment(TEST + CONTAINER_1_NON_JTS)
+	public void test_Ejb2x_StatefulA_Without_Jts() throws Exception {
 		LOG.info("### Beginning test [4] ###");
 		String lookup = "ejb:ear-module-a-1.0-SNAPSHOT/ejb-module-a-1.0-SNAPSHOT/Ejb2x_StatefulA!my.test.api.TestCaseRemote?stateful";
 		TestCaseRemote testCase = (TestCaseRemote)ejb3xBeanLocator.locateBean(lookup);
 		Assert.assertNotNull(testCase);
-		testCase.setUp();
-		testCase.runTest(CONTAINER_3_OFFSET, CONTAINER_4_OFFSET);
-		testCase.tearDown();
+		try {
+			testCase.setUp();
+			String actualResult = testCase.runTest(CONTAINER_1_OFFSET, CONTAINER_2_OFFSET);
+			Assert.assertEquals(FAILED, actualResult);
+		}catch (IllegalStateException ex){
+			LOG.info("### Received expected exception ###");
+		}finally{
+			testCase.tearDown();
+		}
 		LOG.info("### Finished test [4] ###");
 	}
-	
+
+	@Test
+	@OperateOnDeployment(TEST + CONTAINER_3_JTS)
+	public void test_Ejb3x_StatelessA_With_Jts() throws Exception {
+		LOG.info("### Beginning test [5] ###");
+		String lookup = "ejb:ear-module-a-1.0-SNAPSHOT/ejb-module-a-1.0-SNAPSHOT/Ejb3x_StatelessA!my.test.api.TestCaseRemote";
+		TestCaseRemote testCase = (TestCaseRemote)ejb3xBeanLocator.locateBean(lookup);
+		Assert.assertNotNull(testCase);
+		try {
+			testCase.setUp();
+			testCase.runTest(CONTAINER_3_OFFSET, CONTAINER_4_OFFSET);
+			fail("Should have got EJBTransactionRolledbackException here!");
+		}catch (EJBTransactionRolledbackException ex){
+			LOG.info("### Received expected exception ###");
+		}finally{
+			testCase.tearDown();
+		}
+		LOG.info("### Finished test [5] ###");
+	}
 
 
-	
-	
+	@Test
+	@OperateOnDeployment(TEST + CONTAINER_3_JTS)
+	public void test_Ejb3x_StatefulA_With_Jts() throws Exception {
+		LOG.info("### Beginning test [6] ###");
+		String lookup = "ejb:ear-module-a-1.0-SNAPSHOT/ejb-module-a-1.0-SNAPSHOT/Ejb3x_StatefulA!my.test.api.TestCaseRemote?stateful";
+		TestCaseRemote testCase = (TestCaseRemote)ejb3xBeanLocator.locateBean(lookup);
+		Assert.assertNotNull(testCase);
+		try {
+			testCase.setUp();
+			testCase.runTest(CONTAINER_3_OFFSET, CONTAINER_4_OFFSET);
+			fail("Should have got NamingException here!");
+		}catch (NamingException ex){
+			LOG.info("### Received expected exception ###");
+		}finally{
+			testCase.tearDown();
+		}
+		LOG.info("### Finished test [6] ###");
+	}
+
+	@Test
+	@OperateOnDeployment(TEST + CONTAINER_3_JTS)
+	public void test_Ejb2x_StatelessA_With_Jts() throws Exception {
+		LOG.info("### Beginning test [7] ###");
+		String lookup = "ejb:ear-module-a-1.0-SNAPSHOT/ejb-module-a-1.0-SNAPSHOT/Ejb2x_StatelessA!my.test.api.TestCaseRemote";
+		TestCaseRemote testCase = (TestCaseRemote)ejb3xBeanLocator.locateBean(lookup);
+		Assert.assertNotNull(testCase);
+		testCase.setUp();
+		String actualResult = testCase.runTest(CONTAINER_3_OFFSET, CONTAINER_4_OFFSET);
+		Assert.assertEquals(PASSED, actualResult);
+		testCase.tearDown();
+		LOG.info("### Finished test [7] ###");
+	}
+
+
+	@Test
+	@OperateOnDeployment(TEST + CONTAINER_3_JTS)
+	public void test_Ejb2x_StatefulA_With_Jts() throws Exception {
+		LOG.info("### Beginning test [8] ###");
+		String lookup = "ejb:ear-module-a-1.0-SNAPSHOT/ejb-module-a-1.0-SNAPSHOT/Ejb2x_StatefulA!my.test.api.TestCaseRemote?stateful";
+		TestCaseRemote testCase = (TestCaseRemote)ejb3xBeanLocator.locateBean(lookup);
+		Assert.assertNotNull(testCase);
+		testCase.setUp();
+		String actualResult = testCase.runTest(CONTAINER_3_OFFSET, CONTAINER_4_OFFSET);
+		Assert.assertEquals(PASSED, actualResult);
+		testCase.tearDown();
+		LOG.info("### Finished test [8] ###");
+	}
+
+
+
+
+
 }
